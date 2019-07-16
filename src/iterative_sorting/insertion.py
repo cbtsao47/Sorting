4e3c46d3b867261bf9c8cab6eb959a666f759993
Lambda_School_Loaner_62 <lambda_school_loaner_62@Lambda-School-Loaner-62s-MacBook-Pro.local>
2019-07-16 17:54:34 -0500

    add notes

--- src/iterative_sorting/insertion.py
+++ src/iterative_sorting/insertion.py
@@ -6,7 +6,7 @@ def insertion(arr):
         current_value=arr[i]
         # grab the position of the current index
         position=i
-        # if the current value < the left neighboring value
+        # if the current position is not the first element and current value < the left neighboring value
         while(position>0 and current_value<arr[position-1]):
                 # set current index to be the neighboring value
             arr[position]=arr[position-1]
